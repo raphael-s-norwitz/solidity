c6c165942405b37c6027f09bfa5623782a37c055
killianrutherford <krr2125@columbia.edu>
2018-04-17 19:14:04 +0000

    added binary output to different folders

--- parallelisation_test/select_contracts_2.cpp
+++ parallelisation_test/select_contracts_2.cpp
@@ -19,9 +19,9 @@ using namespace std;
 /*
  *run system with compiler, flags, and path and remains 
 */
-int f(string compiler, string flags, string path, string remains) 
+int f(string compiler, string output, string flags, string path, string remains) 
 {
-  string cmd = compiler + flags + path + remains;
+  string cmd = compiler + output + flags + path + remains;
   system(cmd.c_str());
   return 0;
 }
@@ -42,15 +42,15 @@ int f_test(string compiler, string flags, string path, string remains)
  *wrapper to force time-out: source: https://stackoverflow.com/questions/40550730/how-to-implement-timeout-for-function-in-c
  *TODO: source is stack overflow, is there a more efficient way to do it with async?
 */
-int f_wrapper(string compiler, string flags, string path, string remains)
+int f_wrapper(string compiler, string output, string flags, string path, string remains)
 {
   std::mutex m;
   std::condition_variable cv;
   int retValue;
 
-  std::thread t([&m, &cv, &retValue, &compiler, &flags, &path, &remains]() 
+  std::thread t([&m, &cv, &retValue, &compiler, &output, &flags, &path, &remains]() 
       {
-      retValue = f(compiler, flags, path, remains);
+      retValue = f(compiler, output, flags, path, remains);
       cv.notify_one();
       });
 
@@ -71,7 +71,7 @@ int f_wrapper(string compiler, string flags, string path, string remains)
 */
 int f_test2(string compiler, string flags, string path) {
   string cmd = compiler + flags + path + ") 2> /dev/null";
-  //cerr << cmd << '\n';
+  cerr << cmd << '\n';
   array<char, 128> buffer;
   string res;
   shared_ptr<FILE> pipe(popen(cmd.c_str(), "r"), pclose);
@@ -83,6 +83,7 @@ int f_test2(string compiler, string flags, string path) {
       res += buffer.data();
     }
   }
+  //cout << res << '\n';
   return res.find("Binary");
 }
 
@@ -122,7 +123,7 @@ void join_contracts(string compiler, string select_flag,
  *time testing single thread vs multi-thread using async
  */
 void time_test(const vector<string> &sol_files, string test_string, 
-	string compiler, string flags, string remains)
+	string compiler, string flags, string remains, string output_single, string output_multi)
 {
   //obtain num cpus
   int cpus = std::thread::hardware_concurrency();
@@ -135,13 +136,13 @@ void time_test(const vector<string> &sol_files, string test_string,
  
   //single thread execution
   auto t1 = std::chrono::high_resolution_clock::now();
-  f(compiler, flags, test_string, remains);
+  f(compiler, output_single, flags, test_string, remains);
   auto t2 = std::chrono::high_resolution_clock::now();
   
   //multi thread instantiation
   auto t3 = std::chrono::high_resolution_clock::now();
   if (cpus <= 1 || sol_files.size() == 1) {
-    f(compiler, flags, test_string, remains);
+    f(compiler, output_multi, flags, test_string, remains);
   } else {
     int task_for_each_cpu;
     if (sol_files.size() < cpus) { 
@@ -170,7 +171,7 @@ void time_test(const vector<string> &sol_files, string test_string,
     t3 = std::chrono::high_resolution_clock::now();
     //TODO: is this the most efficient way, i.e. cycle through all asyncs and get
     for (auto task : tasks) {
-      vec_res.push_back(async(std::launch::async, f, compiler, flags, task, remains));
+      vec_res.push_back(async(std::launch::async, f, compiler, output_multi, flags, task, remains));
     }
     for (int k = 0; k < vec_res.size(); k++) {
       vec_res[k].get();
@@ -193,10 +194,13 @@ int main(int argc, char** argv)
   }
 
   string compiler = "(../build/solc/solc ";
+  string flags1 ="--optimize --ignore-missing --bin ";
   string flags ="--optimize --ignore-missing --combined-json abi,asm,ast,bin,bin-runtime,clone-bin,compact-format,devdoc,hashes,interface,metadata,opcodes,srcmap,srcmap-runtime,userdoc ";
   string select_flag = "--bin ";
   //string files1 = "*.sol ";
   //string files2 = "*/*.sol";
+  string output_single = "-o single_thread/ ";
+  string output_multi = "-o multi_thread/ ";
 
   string remains = ") >/dev/null 2>&1";
 
@@ -229,6 +233,6 @@ int main(int argc, char** argv)
   join_contracts(compiler, select_flag, path, test_string, sol_files, N);
   
   cout << test_string << '\n';
-  time_test(sol_files, test_string, compiler, flags, remains);
+  time_test(sol_files, test_string, compiler, flags1, remains, output_single, output_multi);
   return 0; 
 }
